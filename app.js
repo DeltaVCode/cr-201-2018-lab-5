6b519d70d29e76f691fb570fe0a9205e6528903b
Andy Fiedler <andyfidd@gmail.com>
2018-06-08 11:55:38 -0500

    Draft1Day5

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
+var result = a + b;
+var array = [result];
+console.log(array);
 
+array[1] = 'the sum of ' + a + ' and ' + b ' is ' + result + ' . '
+
+return array;
 }
 
 // Here is the test for sum(); uncomment it to run it
