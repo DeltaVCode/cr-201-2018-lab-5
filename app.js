331034d45d2651cd79bc2efc5b5e6af809b44c64
Ben Van Meter <vanmeterx@gmail.com>
2018-06-08 14:49:07 -0500

    Attempting to get problem 2 done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) {
+function sum(a, b) { 
   var result = a + b;
   var array = [result];
   console.log(array);
@@ -36,11 +36,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
+  var result = a * b;
+  var array = [result];
+  console.log(array);
 
+  return array;
 }
 
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+//testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
