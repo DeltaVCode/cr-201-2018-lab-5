7274d39c0b7b45089192678369e902c009bbdc82
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-06-08 11:49:43 -0500

    Solved problem 1

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
-
+    var theSum = a + b;
+    var concatString = "The sum of " + a + " and " + b + " is " + theSum + ".";
+    var result = [theSum, concatString];
+    return result;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
