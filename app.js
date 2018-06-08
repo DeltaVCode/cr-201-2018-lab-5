df6195102d81f76cf62788783d64dfe003681948
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-06-08 12:37:20 -0500

    Multiply program done

--- app.js
+++ app.js
@@ -33,11 +33,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
-
+  var result=a*b;
+  var array=[result,"The product of "+a+" and "+b+" is "+result+"."];
+  return array;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
