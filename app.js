2bac0d52396e098ea8bd44aa5f8eb622c7ac669f
Andy Fiedler <andyfidd@gmail.com>
2018-06-08 13:50:54 -0500

    doneProblem1

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ var result = a + b;
 var array = [result];
 console.log(array);
 
-array[1] = 'the sum of ' + a + ' and ' + b ' is ' + result + ' . '
+array[1] = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.'
 
 return array;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -37,7 +37,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
+var result = multiply(a + b);
+var array = [result];
+console.log(array);
 
+array[1] = 'the sum of ' + a + ' and ' + b +' is ' + result + ' . '
+return array
 }
 
 // Here is the test for multiply(); uncomment it to run it
