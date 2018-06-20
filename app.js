e6ceae95120c7f9c2240beefeec80305ea4fe695
JohnMV13 <thejohnmiesterV@gmail.com>
2018-06-19 22:18:26 -0500

    problem-5 is working correctly

--- app.js
+++ app.js
@@ -91,16 +91,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumArray(sumArr) {
-    var sSum = 0
-    for(var i = 0; i < sumArr.length; i++) {
-        var sSum = sum(sumArr[i], sSum)[0];
-
-        console.log(sSum)
-    
+    var sSum = [];
+    sSum[0] = sum(sumArr[0],sumArr[1])[0];
+    var i = 2;
+    while (i<(sumArr.length)){
+       sSum[0] = sum(sSum[0],sumArr[i])[0];
+        i++;
     }
-    var array = [sumArr]
-    array[1] = sumArr + ' was passed in as an array of numbers, and ' + sSum + ' is their sum.';
-    return array;
+    sSum[1] = sumArr + ' was passed in as an array of numbers, and ' + sSum[0] + ' is their sum.';
+    return sSum;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -121,14 +120,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
-    var mMult = multiply(multArr[0], multArr[1])[0];
-    for (var i = 2; i < multArr.length; i++) {
-       mMult =multiply(multArr[i], mMult)[0];
-        console.log(mMult);
+    var mMult = [];
+    mMult[0] = multiply(multArr[0],multArr[1])[0];
+    var i = 2;
+    while (i<(multArr.length)){
+        mMult[0] = multiply(mMult[0],multArr[i])[0];
+        i++;
     }
-    var array = [mMult];
-    array[1] = "The numbers " + multArr + ", have the product of " + mMult + "."
-    return array  
+    mMult[1] = "The numbers " + multArr + " have a product of " + mMult[0] + ".";
+    return mMult;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
